91ed7e8e3496b862c0a28e845c25a0a137e10a41
andreyhenriquetw <andreyhenriquetw2@gmail.com>
2025-08-12 20:55:40 -0300

    feat: add second page.tsx banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -24,15 +24,25 @@ export default async function Home() {
         alt="Até 55% de desconto esse mês!"
       />
 
-      {/* Div das categorias de Texto  */}
+      {/* Div das categorias de Texto  com Icones */}
       <div className="mt-8 px-5">
         <Categories />
       </div>
 
       {/* Div dos Produtos de Imagem  */}
       <div className="mt-8">
+        <p className="mb-3 pl-5 font-bold uppercase">Ofertas</p>
         <ProductList products={deals} />
       </div>
+
+      <Image
+        src="/banner-home-2.png"
+        height={0}
+        width={0}
+        className="h-auto w-full px-5"
+        sizes="100vw"
+        alt="Até 55% de desconto em mouses!"
+      />
     </div>
   );
 }
