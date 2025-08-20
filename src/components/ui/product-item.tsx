f13e3522d68e07fe0a9ecccbbc6fcf4fa3194133
andreyhenriquetw <andreyhenriquetw2@gmail.com>
2025-08-19 21:06:03 -0300

    feat: ensure user goes to product details page on product click

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -2,6 +2,7 @@ import { ProductWithTotalPrice } from "@/helpers/product";
 import Image from "next/image";
 import { Badge } from "./badge";
 import { ArrowDownIcon, Star } from "lucide-react";
+import Link from "next/link";
 
 interface ProductItemProps {
   product: ProductWithTotalPrice;
@@ -10,71 +11,73 @@ interface ProductItemProps {
 // Exibe a imagem e o nome do produto
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <div className="flex flex-col gap-4">
-      <div className="relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent">
-        <Image
-          src={product.imageUrls[0]}
-          height={0}
-          width={0}
-          sizes="100vw"
-          className="h-auto max-h-[70%] w-auto max-w-[80%]"
-          style={{
-            objectFit: "contain",
-          }}
-          alt={product.name}
-        />
+    <Link href={`/product/${product.slug}`}>
+      <div className="flex flex-col gap-4">
+        <div className="relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent">
+          <Image
+            src={product.imageUrls[0]}
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto max-h-[70%] w-auto max-w-[80%]"
+            style={{
+              objectFit: "contain",
+            }}
+            alt={product.name}
+          />
 
-        {/* Div do Desconto % dentro da image */}
-        {product.discountPercentage > 0 && (
-          <Badge className="absolute left-3 top-3 px-2 py-[2px]">
-            <ArrowDownIcon size={14} /> {product.discountPercentage}%
-          </Badge>
-        )}
-      </div>
-
-      {/* Nome dos Produtos */}
-      <div className="flex flex-col gap-1">
-        <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
-          {product.name}
-        </p>
-
-        {/* Nome dos Preços */}
-        <div className="flex items-center gap-2">
-          {product.discountPercentage > 0 ? (
-            <>
-              <p className="overflow-hidden text-ellipsis whitespace-nowrap font-semibold">
-                R$ {product.totalPrice.toFixed(2)}
-              </p>
-
-              <p className="overflow-hidden text-ellipsis whitespace-nowrap text-xs line-through opacity-75">
-                R$ {Number(product.basePrice).toFixed(2)}
-              </p>
-            </>
-          ) : (
-            <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm font-semibold">
-              R$ {product.basePrice.toFixed(2)}
-            </p>
+          {/* Div do Desconto % dentro da image */}
+          {product.discountPercentage > 0 && (
+            <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+              <ArrowDownIcon size={14} /> {product.discountPercentage}%
+            </Badge>
           )}
         </div>
 
-        {/* Avaliação - 4 cheias e 1 vazia */}
-        <div className="flex items-center gap-[3px]">
-          {[1, 2, 3, 4].map((star) => (
+        {/* Nome dos Produtos */}
+        <div className="flex flex-col gap-1">
+          <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+            {product.name}
+          </p>
+
+          {/* Nome dos Preços */}
+          <div className="flex items-center gap-2">
+            {product.discountPercentage > 0 ? (
+              <>
+                <p className="overflow-hidden text-ellipsis whitespace-nowrap font-semibold">
+                  R$ {product.totalPrice.toFixed(2)}
+                </p>
+
+                <p className="overflow-hidden text-ellipsis whitespace-nowrap text-xs line-through opacity-75">
+                  R$ {Number(product.basePrice).toFixed(2)}
+                </p>
+              </>
+            ) : (
+              <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm font-semibold">
+                R$ {product.basePrice.toFixed(2)}
+              </p>
+            )}
+          </div>
+
+          {/* Avaliação - 4 cheias e 1 vazia */}
+          <div className="flex items-center gap-[3px]">
+            {[1, 2, 3, 4].map((star) => (
+              <Star
+                key={star}
+                size={14}
+                strokeWidth={2}
+                className="fill-[#5c3dd9] text-[#5c3dd9]"
+              />
+            ))}
             <Star
-              key={star}
               size={14}
               strokeWidth={2}
-              className="fill-[#5c3dd9] text-[#5c3dd9]"
+              className="text-[#5c3dd9]" // vazia
             />
-          ))}
-          <Star
-            size={14}
-            strokeWidth={2}
-            className="text-[#5c3dd9]" // vazia
-          />
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
