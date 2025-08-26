e1998f2d40979c8107861070cb9c6db9bd43a3d8
andreyhenriquetw <andreyhenriquetw2@gmail.com>
2025-08-25 23:24:30 -0300

    feat: add CartContext

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/ui/header";
 import Footer from "@/components/ui/footer";
+import CartProvider from "@/providers/cart";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,9 +21,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="flex h-full flex-col">
-          <Header />
-          <div className="flex-1">{children}</div>
-          <Footer />
+          <CartProvider>
+            <Header />
+            <div className="flex-1">{children}</div>
+            <Footer />
+          </CartProvider>
         </div>
       </body>
     </html>
