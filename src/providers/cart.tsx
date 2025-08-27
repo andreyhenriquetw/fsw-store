7ed718fd498f881b8c7f3b778bbf0cb3f21f5244
andreyhenriquetw <andreyhenriquetw2@gmail.com>
2025-08-27 16:55:27 -0300

    feat: ensure product quantity can be  modified on cart

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -13,6 +13,7 @@ interface ICartContext {
   cartBasePrice: number;
   cartTotalDiscount: number;
   addProductToCart: (product: CartProduct) => void;
+  decreseProductQuantity: (productId: string) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -21,6 +22,7 @@ export const CartContext = createContext<ICartContext>({
   cartBasePrice: 0,
   cartTotalDiscount: 0,
   addProductToCart: () => {},
+  decreseProductQuantity: () => {},
 });
 
 const CartProvider = ({ children }: { children: ReactNode }) => {
@@ -47,14 +49,36 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
 
       return;
     }
-
+    // Se não, adicionar o producto  a lista
     setProducts((prev) => [...prev, product]);
+  };
+
+  const decreseProductQuantity = (productId: string) => {
+    // se a quantidade for 1, remova o produto do carrinho
+
+    //  se não, diminua a quantidade em 1
+
+    setProducts((prev) =>
+      prev
+        .map((cartProduct) => {
+          if (cartProduct.id === productId) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity - 1,
+            };
+          }
+
+          return cartProduct;
+        })
+        .filter((cartProduct) => cartProduct.quantity > 0),
+    );
   };
   return (
     <CartContext.Provider
       value={{
         products,
         addProductToCart,
+        decreseProductQuantity,
         cartTotalPrice: 0,
         cartBasePrice: 0,
         cartTotalDiscount: 0,
